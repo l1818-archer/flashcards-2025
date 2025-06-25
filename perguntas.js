79e4ccc42682d45b31f43bdd19e3cb54fdc4b785
l1818-archer <camila.antoska@escola.pr.gov.br>
2025-06-25 15:52:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Quimica',
+    'A água é uma substância composta?',
+    'Sim, é formada por 2 elementos, hidrogênio e oxigênio'
+)
+
+criaCartao(
+    'Quimica',
+    'Todos os ácidos possuem hidrogênio na sua fórmula?',
+    'Sim, pois liberam íons H+ em solução aquosa'
+)
+
+criaCartao(
+    'Quimica',
+    'O sal de cozinha é uma mistura?',
+    'Não, é uma substância pura composta'
+)
+
+criaCartao(
+    'Quimica',
+    'Metais conduzem eletricidade no estado sólido?',
+    'Sim'
+)
+
+criaCartao(
+    'Quimica',
+    'A queima de papel é uma mudança física?',
+    'Não, é uma mudança'
+)
+
+criaCartao(
+    'Quimica',
+    'O oxigênio é necessário em todas as reações de combustão?',
+    'Sim'
+)
